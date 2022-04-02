1598f911b43bb5b3724c38dbf5ec4e4513f03bf2
Leonel-20-Oswaldo <85091225+Leonel-20-Oswaldo@users.noreply.github.com>
2022-04-01 20:42:39 -0600

    Tercera version

--- mobile-project/app/components/Account/RegisterForm.js
+++ mobile-project/app/components/Account/RegisterForm.js
@@ -1,33 +1,77 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { StyleSheet, View, Text} from 'react-native'
 import {Input, Icon, Button} from 'react-native-elements'
+import { validateEmail } from '../../utils/validation'
 
 export default function RegisterForm(){
+    const [showPassword, setShowPassword] = useState(false)
+    const [showRepeatPassword, setShowRepeatPassword] = useState(false)
+    const [formData, setFormData] = useState(defaultFormValues())
+
+    const onSubmint = () => {
+        if(formData.email.length===0||formData.password.length===0||formData.repeatPassword.length===0){
+            console.log('Todos los campos son requeridos')
+        } else if (!validateEmail(formData.email)){
+            console.log('El email no es correcto')
+        } else if (formData.password !== formData.repeatPassword){
+            console.log('Las contraseñas deben ser identicas')
+        } else if (formData.password.length < 6){
+            console.log('El password debe tener minimo 6 caracteres')
+        } else{
+            console.log('Todo esta bien, listo')
+        }
+    }
+
+    const onChange = (e, type) => {
+        setFormData({ ...formData, [type]: e.nativeEvent.text})
+    }
+
     return(
         <View style={styles.formContainer}>
             <Input
                 placeholder='Correo electronico'
                 containerStyle={styles.inputForm}
+                onChange={(e)=>onChange(e,'email')}
+                rightIcon={<Icon type='material-community' name='at' iconStyle={styles.iconRight}/>}
             />
             <Input
                 placeholder='Contraseña'
                 containerStyle={styles.inputForm}
                 password={true}
-                secureTextEntry={true}
+                secureTextEntry={showPassword ? false : true}
+                onChange={(e)=>onChange(e,'password')}
+                rightIcon={<Icon 
+                    type='material-community' 
+                    name={showPassword ? 'eye-off-outline' : 'eye-outline'} 
+                    iconStyle={styles.iconRight} onPress={()=> setShowPassword(!showPassword)} />}
             />
             <Input
                 placeholder='Repetir Contraseña'
                 containerStyle={styles.inputForm}
                 password={true}
-                secureTextEntry={true}
+                secureTextEntry={showRepeatPassword ? false : true}
+                onChange={(e)=>onChange(e,'repeatPassword')}
+                rightIcon={<Icon 
+                    type='material-community' 
+                    name={showRepeatPassword ? 'eye-off-outline' : 'eye-outline'} 
+                    iconStyle={styles.iconRight} onPress={()=> setShowRepeatPassword(!showRepeatPassword)} />}
             />
             <Button
                 title='Unete'
                 containerStyle={styles.btnContainerRegister}
                 buttonStyle={styles.btnRegister}
+                onPress={onSubmint}
             />
         </View>
     )
+}
+
+function defaultFormValues(){
+    return{
+        email:'',
+        password:'',
+        repeatPassword:''
+    }
 }
 
 const styles = StyleSheet.create({
@@ -44,6 +88,9 @@ const styles = StyleSheet.create({
     },
     btnRegister:{
         backgroundColor:'#00a680'
+    },
+    iconRight:{
+        color:'#c1c1c1'
     }
 
 })
