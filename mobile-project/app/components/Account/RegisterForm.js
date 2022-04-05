131780348db53511aa35b5ce5266a60270d0a3aa
Leonel-20-Oswaldo <85091225+Leonel-20-Oswaldo@users.noreply.github.com>
2022-04-04 22:09:45 -0500

    Actualizando

--- mobile-project/app/components/Account/RegisterForm.js
+++ mobile-project/app/components/Account/RegisterForm.js
@@ -2,23 +2,63 @@ import React, {useState} from 'react'
 import { StyleSheet, View, Text} from 'react-native'
 import {Input, Icon, Button} from 'react-native-elements'
 import { validateEmail } from '../../utils/validation'
+import firebase from 'firebase'
+import { useNavigation } from '@react-navigation/native'
 
-export default function RegisterForm(){
+export default function RegisterForm(props){
+    const {toastRef} = props
     const [showPassword, setShowPassword] = useState(false)
     const [showRepeatPassword, setShowRepeatPassword] = useState(false)
     const [formData, setFormData] = useState(defaultFormValues())
+    const navigation = useNavigation()
 
     const onSubmint = () => {
         if(formData.email.length===0||formData.password.length===0||formData.repeatPassword.length===0){
-            console.log('Todos los campos son requeridos')
+            toastRef.current.show({
+                type: 'error',
+                position: 'top',
+                text1: 'Notificación',
+                text2: 'Todos los campos son requeridos',
+                visibilityTime: 3000,
+            });
         } else if (!validateEmail(formData.email)){
-            console.log('El email no es correcto')
+            toastRef.current.show({
+                type: 'error',
+                position: 'top',
+                text1: 'Notificación',
+                text2: 'El email no es correcto',
+                visibilityTime: 3000,
+            });
         } else if (formData.password !== formData.repeatPassword){
-            console.log('Las contraseñas deben ser identicas')
+            toastRef.current.show({
+                type: 'error',
+                position: 'top',
+                text1: 'Notificación',
+                text2: 'Las contraseñas deben ser identicas',
+                visibilityTime: 3000,
+            });
         } else if (formData.password.length < 6){
-            console.log('El password debe tener minimo 6 caracteres')
+            toastRef.current.show({
+                type: 'error',
+                position: 'top',
+                text1: 'Notificación',
+                text2: 'El password debe tener minimo 6 caracteres',
+                visibilityTime: 3000,
+            });
         } else{
-            console.log('Todo esta bien, listo')
+            firebase.auth().createUserWithEmailAndPassword(formData.email, formData.password)
+            .then((response)=>{
+                navigation.navigate('account')
+            })
+            .catch(()=>{
+                toastRef.current.show({
+                    type: 'error',
+                    position: 'top',
+                    text1: 'Cuenta',
+                    text2: 'Este correo ya ha sido registrado',
+                    visibilityTime: 3000,
+                });
+            })
         }
     }
 
--- mobile-project/app/screens/Account/Regiter.js
+++ mobile-project/app/screens/Account/Regiter.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, {useRef} from "react";
 import { StyleSheet, View, Text, Image } from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import RegisterForm from "../../components/Account/RegisterForm";
+import Toast from 'react-native-toast-message';
 
 
 export default function Register(){
+    const toastRef = useRef()
     return(
         <KeyboardAwareScrollView>
             <Image source={require('../../../assets/img/restaurant.png')} resizeMode='contain' style={styles.logo}
                 />
              <View style ={styles.viewForms}>
-                 <RegisterForm/>
+                 <RegisterForm toastRef={toastRef}/>
             </View>
+            <Toast ref={toastRef}/>
         </KeyboardAwareScrollView>
     )
 }
